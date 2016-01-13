6926b4ddb055d2a95da2c9602f4af5f21dfa372d
Laszlo Bogardi <Laszlo.Bogardi@ams-ix.net>
2016-01-13 21:53:39 +0100

    PID autotune values for both bed and hotend.

--- kossel/Configuration.h
+++ kossel/Configuration.h
@@ -209,9 +209,9 @@ Here are some standard links for getting your machine calibrated:
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+  #define  DEFAULT_Kp 22.08
+  #define  DEFAULT_Ki 1.36
+  #define  DEFAULT_Kd 89.55
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
@@ -255,9 +255,13 @@ Here are some standard links for getting your machine calibrated:
 
   //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
+  //#define  DEFAULT_bedKp 10.00
+  //#define  DEFAULT_bedKi .023
+  //#define  DEFAULT_bedKd 305.4
+
+  #define  DEFAULT_bedKp 396.03
+  #define  DEFAULT_bedKi 69.45
+  #define  DEFAULT_bedKd 564.54
 
   //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
